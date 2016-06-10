7fa98230f5337f4e6c6bf5fca88b3993aa484d16
Michael Krukar <krukar@gmail.com>
2016-06-09 21:32:28 -0400

    Finalized layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,32 @@
-///<reference path="jquery.d.ts" />
+///<reference path="d/jquery.d.ts" />
 'use strict';
 $(document).ready(function () {
-    var total = getWeeks();
-    var list = $('#media');
-    var weeks = '';
-    for (var i = 0; i < total; i++) {
-        var week = '<li class="day monday"></li><li class="day tuesday"></li><li class="day wednesday"></li><li class="day thursday"></li><li class="day friday"></li><li class="day saturday"></li><li class="day sunday"></li>';
-        weeks += week;
-    }
-    list.html(weeks);
+    var media = $('#media');
     $.getJSON('data/media.json', function (data) {
         data.books.forEach(function (book) {
             var date = new Date(book.date);
             var dayNumber = getDayNumber(date);
-            $('li.day:eq(' + dayNumber + ')', list).addClass('book');
+            console.log(dayNumber);
+            $('li.day:eq(' + dayNumber + ')', media).addClass('book');
         });
         data.games.forEach(function (game) {
             var date = new Date(game.date);
             var dayNumber = getDayNumber(date);
-            $('li.day:eq(' + dayNumber + ')', list).addClass('game');
+            $('li.day:eq(' + dayNumber + ')', media).addClass('game');
         });
         data.movies.forEach(function (movie) {
             var date = new Date(movie.date);
             var dayNumber = getDayNumber(date);
-            $('li.day:eq(' + dayNumber + ')', list).addClass('movie');
+            $('li.day:eq(' + dayNumber + ')', media).addClass('movie');
         });
-        $('.day.book, .day.game, .day.movie').hover(function () {
-            $('.day').addClass('fade');
-            $(this).addClass('active');
+        var items = $('.day.book, .day.game, .day.movie');
+        items.hover(function () {
+            items.not(this).addClass('fade');
         }, function () {
-            $('.day').removeClass('fade');
-            $(this).removeClass('active');
+            items.not(this).removeClass('fade');
         });
-    });
-    list.on('click', function () {
-        $(this).toggleClass('border');
     });
 });
-function getWeeks() {
-    var total = Math.round(Math.abs((new Date(2013, 0, 1).getTime() - new Date((new Date().getFullYear() + 1), 0, 1).getTime()) / (24 * 7 * 60 * 60 * 1000)));
-    return total;
-}
 function getDayNumber(date) {
     var day = Math.round(Math.abs((new Date(2013, 0, 1).getTime() - date.getTime()) / (24 * 60 * 60 * 1000)));
     return day;
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,60 +1,44 @@
-///<reference path="jquery.d.ts" />
+///<reference path="d/jquery.d.ts" />
 
 'use strict';
 
 $(document).ready(() => {
 
-	let total: number = getWeeks();
-	let list: any = $('#media');
-	let weeks: string = '';
-
-	for(let i = 0; i < total; i++){
-		let week: string = '<li class="day monday"></li><li class="day tuesday"></li><li class="day wednesday"></li><li class="day thursday"></li><li class="day friday"></li><li class="day saturday"></li><li class="day sunday"></li>';
-		weeks += week;
-	}
-
-	list.html(weeks);
+	let media: any = $('#media');
 
 	$.getJSON('data/media.json', function(data: any) {
 
 		data.books.forEach(function(book: any) {
 			let date: Date = new Date(book.date); 
 			let dayNumber: number = getDayNumber(date);
-			$('li.day:eq(' + dayNumber + ')', list).addClass('book');
+			console.log(dayNumber)
+			$('li.day:eq(' + dayNumber + ')', media).addClass('book');
 		});
 
 		data.games.forEach(function(game: any) {
 			let date: Date = new Date(game.date); 
 			let dayNumber: number = getDayNumber(date);
-			$('li.day:eq(' + dayNumber + ')', list).addClass('game');
+			$('li.day:eq(' + dayNumber + ')', media).addClass('game');
 		});
 
 		data.movies.forEach(function(movie: any) {
 			let date: Date = new Date(movie.date); 
 			let dayNumber: number = getDayNumber(date);
-			$('li.day:eq(' + dayNumber + ')', list).addClass('movie');
+			$('li.day:eq(' + dayNumber + ')', media).addClass('movie');
 		});
 
-		$('.day.book, .day.game, .day.movie').hover(function(){
-			$('.day').addClass('fade');
-			$(this).addClass('active');
+		let items: any = $('.day.book, .day.game, .day.movie');
+
+		items.hover(function(){
+			items.not(this).addClass('fade');
+			
 		},function(){
-			$('.day').removeClass('fade');
-			$(this).removeClass('active');
+			items.not(this).removeClass('fade');
 		});
 
-	});
-
-	list.on('click', function(){
-		$(this).toggleClass('border');
 	});
 
 });
-
-function getWeeks(){
-	let total: number = Math.round(Math.abs((new Date(2013, 0, 1).getTime() - new Date((new Date().getFullYear() + 1), 0, 1).getTime()) / (24 * 7 * 60 * 60 * 1000)));
-	return total;
-}
 
 function getDayNumber(date : Date){
 	let day: number = Math.round(Math.abs((new Date(2013, 0, 1).getTime() - date.getTime()) / (24 * 60 * 60 * 1000)));
